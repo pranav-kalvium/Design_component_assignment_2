885e305831f00b51689e02505940d506bfe25d6e
Pranav-cse-kalvium <pranav.prakash@kalvium.community>
2025-02-21 09:20:43 +0530

    pushed

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,14 @@
-// write button card here
+
+import React from 'react'
+
+const button = () => {
+  return (
+    <div>
+        <button className="mt-3 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+      View Product
+    </button>
+    </div>
+  )
+}
+
+export default button
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,19 @@
 // write product card here
+import React from 'react'
+import Button from './button'
+
+const productcard = () => {
+    const productName = "Wireless Headphones";
+    const productImage = "https://www.bhphotovideo.com/images/images2000x2000/sony_whch700n_b_ch700n_wireless_noise_cancelling_headphones_1389412.jpg";
+    const productPrice = "$99.99";
+  return (
+    <div className="border p-4 rounded-lg shadow-md max-w-sm bg-white">
+    <img style={{height: '180px', width: '100%'}} src={productImage} alt={productName} className="w-full h-48 object-cover rounded-md" />
+    <h2 className="text-lg font-semibold mt-2">{productName}</h2>
+    <p className="text-gray-600 text-sm">Price: {productPrice}</p>
+    <Button />
+    </div>
+  )
+}
+
+export default productcard
